94d580cf8f0bd98c416a13ee8e00561471932242
klowe1 <46995125+klowe1@users.noreply.github.com>
2019-03-01 11:19:42 +0000

    Add files via upload
    Uploaded again because Gaffney deleted

---
+++ flaskApp/flaskWebsite.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/home") # Tells the browser where to look
+def home():
+    return render_template('home.html') # This points to the html page
+
+@app.route("/about") # Tells the browser where to look
+def How_it_works():
+    return render_template('about.html', title='About') # This points to the html page
+
+@app.route("/subscription") # Tells the browser where to look
+def subscription():
+    return render_template('subscription.html', title='Subscriptions') # This points to the html page
+
+@app.route("/contact") # Tells the browser where to look
+def Contact():
+    return render_template('contact.html', title-'Contact') # This points to the html page
+
+
+if __name__ == '__main__': # This will allow us to run the and change the page without restarting the server
+    app.run(debug=True)
