6df3de66d67ad743615e97e0e23711d9ec6dbfc4
Stephen Gaffney <stephengaffney@ucdconnect.ie>
2019-03-31 20:22:11 +0100

    attempt2

--- flaskApp/flaskWebsite.py
+++ flaskApp/flaskWebsite.py
@@ -52,18 +52,74 @@ def predictive(stuff):
     #stuff = jsonify(stuff)
     return stuff
 
-@app.route('/weather/<timestamp>')
-def weatherForecast(timestamp):
+@app.route('/weatherToday')
+def weatherToday():
+    weather_dict ={}
+    
+    #Current Weather
+    with urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?id=7778677&APPID=0927fd5dff272fdbd486187e54283310") as url:
+        data = json.loads(url.read().decode())
+        
+        weather = data["weather"][0]["main"]
+        icon = data["weather"][0]["icon"]
+        temp = data["main"]["temp"]
+        
+        weather_dict["Now"] = {"weather" : weather, "temp" : temp, "icon" : icon}
+    
+    #Next 3 weather forecasts
     with urllib.request.urlopen("http://api.openweathermap.org/data/2.5/forecast?id=7778677&APPID=0927fd5dff272fdbd486187e54283310") as url:
         data = json.loads(url.read().decode())
         
-        time = data["list"][0]["dt"]
-        temp = data["list"][0]["main"]["temp"]
-        weather_type = data["list"][0]["weather"][0]["main"]
-        icon = data["list"][0]["weather"][0]["icon"]
-        date_time = data["list"][0]["dt_txt"]
+        for i in range(0, 3, +1):
+            print(data["list"][i]["dt_txt"])
+            forecast_date_time = data["list"][i]["dt_txt"]
+            forecast_date = forecast_date_time.split()[0]
+            forecast_time = forecast_date_time.split()[1] 
+            
+            print(forecast_time)
+            icon = data["list"][i]["weather"][0]["icon"]
+            weather = data["list"][i]["weather"][0]["main"]
+            temp = data["list"][i]["main"]["temp"]
+            print(weather, temp, icon)
+            
+            weather_dict[forecast_time] = {"weather" : weather, "temp" : temp, "icon" : icon}
+
         
-    return str(time) + str(temp) + str(weather_type) + str(icon) + str(date_time)
+    for i in weather_dict:
+        print(i,weather_dict[i])
+                  
+    return jsonify(weather_dict)
+
+@app.route('/weatherForecast/<date>')
+def weatherForecast(date):
+    with urllib.request.urlopen("http://api.openweathermap.org/data/2.5/forecast?id=7778677&APPID=0927fd5dff272fdbd486187e54283310") as url:
+        data = json.loads(url.read().decode())
+        weather_dict ={}
+        for i in range(0, len(data["list"]), +1):
+            print(date,"GAP", data["list"][i]["dt_txt"])
+            forecast_date_time = data["list"][i]["dt_txt"]
+            forecast_date = forecast_date_time.split()[0]
+            print(forecast_date)
+            if date == forecast_date:
+                forecast_time = forecast_date_time.split()[1] 
+                print(forecast_time)
+                
+                if forecast_time == '09:00:00' or forecast_time == '12:00:00' or forecast_time == '15:00:00' or forecast_time == '18:00:00':
+                    print(forecast_time)
+                    icon = data["list"][i]["weather"][0]["icon"]
+                    weather = data["list"][i]["weather"][0]["main"]
+                    temp = data["list"][i]["main"]["temp"]
+                    print(weather, temp, icon)
+                    
+                    weather_dict[forecast_time] = {"weather" : weather, "temp" : temp, "icon" : icon}
+                    
+            print()     
+            
+        for i in weather_dict:
+            print(i,weather_dict[i])   
+
+    return jsonify(weather_dict)
+
 
 @app.route("/about") # Tells the browser where to look
 def How_it_works():
